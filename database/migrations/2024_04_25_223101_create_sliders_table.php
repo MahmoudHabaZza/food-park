88ab351932afc96fbf6e05b8fc3904037920e755
Mahmoud Habazza <habazzaofficial@gmail.com>
2024-04-26 01:44:19 +0200

    77 - 3Home Slider Defining migration communes

--- database/migrations/2024_04_25_223101_create_sliders_table.php
+++ database/migrations/2024_04_25_223101_create_sliders_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('offer')->nullable();
+            $table->string('title');
+            $table->string('sub_title');
+            $table->string('description');
+            $table->string('btn_link');
+            $table->enum('status',['0','1'])->default('1');
             $table->timestamps();
         });
     }

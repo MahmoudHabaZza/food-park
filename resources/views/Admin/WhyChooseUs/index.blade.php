4828ded5fd07ad324e48fc133bbfa45e531967be
Mahmoud Habazza <habazzaofficial@gmail.com>
2024-04-28 05:21:27 +0200

    94 - 2Why choose us Creating Necessary Files and Designs Part 2

--- resources/views/Admin/WhyChooseUs/index.blade.php
+++ resources/views/Admin/WhyChooseUs/index.blade.php
@@ -3,10 +3,46 @@
     Why Choose Us
 @endsection
 @section('content')
+
+
     <div class="section">
         <div class="section-header">
             <h1>Why Choose Us</h1>
         </div>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>Why Choose Us</h4>
+            </div>
+            <div class="card-body">
+                      <div class="accordion" id="accordion">
+                        <div class="accordion-header collapsed bg-primary text-light p-3" role="button" data-toggle="collapse" data-target="#panel-body-1" aria-expanded="false">
+                          <h4>Why Choose Us Section Title</h4>
+                        </div>
+                        <div class="accordion-body collapse" id="panel-body-1" data-parent="#accordion" style="">
+                            <form action="" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Top Title</label>
+                                    <input type="text" class="form-control" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input type="text" class="form-control" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Subtitle</label>
+                                    <input type="text" class="form-control" />
+                                </div>
+                                <button class="btn btn-primary" type="submit" >Save</button>
+
+                            </form>
+                        </div>
+                      </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="section">
         <div class="card card-primary">
             <div class="card-header">
                 <h4>Why Choose Us</h4>

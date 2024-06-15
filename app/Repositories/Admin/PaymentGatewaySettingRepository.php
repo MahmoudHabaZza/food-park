87fc04cc85fb80ba085cf3d947d9b28fd9fc3eaf
Mahmoud HabaZza <habazzaofficial@gmail.com>
2024-06-15 18:52:31 +0300

    231 - 3PayPal Setting Working with Form Handling Part 2

--- app/Repositories/Admin/PaymentGatewaySettingRepository.php
+++ app/Repositories/Admin/PaymentGatewaySettingRepository.php
@@ -3,16 +3,52 @@
 namespace App\Repositories\Admin;
 
 use App\Interfaces\Admin\PaymentGatewaySettingRepositoryInterface;
+use App\Models\PaymentGatewaySetting;
+use App\Traits\UploadFileTrait;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PaymentGatewaySettingRepository implements PaymentGatewaySettingRepositoryInterface {
+    use UploadFileTrait;
     public function index() : View
     {
         return view('Admin.Payment-Gateways.index');
     }
     public function paypalSettingsUpdate(Request $request)
     {
-        dd($request->all());
+        $validatedData = $request->validate([
+            'paypal_status' => ['required','boolean'],
+            'paypal_account_mode' => ['required','in:sandbox,live'],
+            'paypal_country' => ['required','string'],
+            'paypal_account_currency' => ['required','string'],
+            'paypal_currency_rate' => ['required','numeric'],
+            'paypal_api_key' => ['required'],
+            'paypal_secret_key' => ['required'],
+        ]);
+
+        if($request->hasFile('paypal_image')){
+            $request->validate([
+                'paypal_image' => ['nullable','image']
+            ]);
+
+            $imagePath = $this->uploadImage($request,'paypal_image');
+            PaymentGatewaySetting::updateOrCreate(
+                ['key' => 'paypal_image'],
+                ['value' => $imagePath]
+            );
+
+        }
+
+
+
+        foreach($validatedData as $key => $value){
+            PaymentGatewaySetting::updateOrCreate(
+                ['key'=> $key],
+                ['value'=> $value]
+            );
+        }
+
+        toastr()->success('Updated Successfully');
+        return redirect()->back();
     }
 }

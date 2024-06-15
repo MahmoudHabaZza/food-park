f2eb8704b74a14f513dbf82e307ef7b4f314093c
Mahmoud HabaZza <habazzaofficial@gmail.com>
2024-06-16 01:48:15 +0300

    232 - 4PayPal Setting Working with Form Handling Part 3

--- app/Repositories/Admin/PaymentGatewaySettingRepository.php
+++ app/Repositories/Admin/PaymentGatewaySettingRepository.php
@@ -7,44 +7,47 @@ use App\Models\PaymentGatewaySetting;
 use App\Traits\UploadFileTrait;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use stdClass;
 
-class PaymentGatewaySettingRepository implements PaymentGatewaySettingRepositoryInterface {
+class PaymentGatewaySettingRepository implements PaymentGatewaySettingRepositoryInterface
+{
     use UploadFileTrait;
-    public function index() : View
+    public function index(): View
     {
-        return view('Admin.Payment-Gateways.index');
+        $paymentSetting = (object) PaymentGatewaySetting::all()->groupBy('key')->toArray();
+
+        return view('Admin.Payment-Gateways.index', compact('paymentSetting'));
     }
     public function paypalSettingsUpdate(Request $request)
     {
         $validatedData = $request->validate([
-            'paypal_status' => ['required','boolean'],
-            'paypal_account_mode' => ['required','in:sandbox,live'],
-            'paypal_country' => ['required','string'],
-            'paypal_account_currency' => ['required','string'],
-            'paypal_currency_rate' => ['required','numeric'],
+            'paypal_status' => ['required', 'boolean'],
+            'paypal_account_mode' => ['required', 'in:sandbox,live'],
+            'paypal_country' => ['required', 'string'],
+            'paypal_account_currency' => ['required', 'string'],
+            'paypal_currency_rate' => ['required', 'numeric'],
             'paypal_api_key' => ['required'],
             'paypal_secret_key' => ['required'],
         ]);
 
-        if($request->hasFile('paypal_image')){
+        if ($request->hasFile('paypal_image')) {
             $request->validate([
-                'paypal_image' => ['nullable','image']
+                'paypal_image' => ['nullable', 'image']
             ]);
 
-            $imagePath = $this->uploadImage($request,'paypal_image');
+            $imagePath = $this->uploadImage($request, 'paypal_image');
             PaymentGatewaySetting::updateOrCreate(
                 ['key' => 'paypal_image'],
                 ['value' => $imagePath]
             );
-
         }
 
 
 
-        foreach($validatedData as $key => $value){
+        foreach ($validatedData as $key => $value) {
             PaymentGatewaySetting::updateOrCreate(
-                ['key'=> $key],
-                ['value'=> $value]
+                ['key' => $key],
+                ['value' => $value]
             );
         }
 
--- resources/views/Admin/Payment-Gateways/sections/paypal.blade.php
+++ resources/views/Admin/Payment-Gateways/sections/paypal.blade.php
@@ -1,85 +1,103 @@
-<div class="tab-pane fade show active" id="paypal-settings" role="tabpanel"
-aria-labelledby="home-tab4">
-<div class="card card-primary border">
-    <div class="card-header">
-        <h5>Update General Settings</h5>
-    </div>
-    <div class="card-body">
-        <form action="{{ route('admin.paypal.settings.update') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Paypal Status</label>
-                        <select name="paypal_status" class="form-control select2">
-                            <option value="1">Active</option>
-                            <option value="0">Inactive</option>
-                        </select>
+<div class="tab-pane fade show active" id="paypal-settings" role="tabpanel" aria-labelledby="home-tab4">
+    <div class="card card-primary border">
+        <div class="card-header">
+            <h5>Update Paypal Settings</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('admin.paypal.settings.update') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Status</label>
+                            <select name="paypal_status" class="form-control select2">
+                                <option @selected($paymentSetting->paypal_status[0]['value'] === '1') value="1">Active</option>
+                                <option @selected($paymentSetting->paypal_status[0]['value'] === '0') value="0">Inactive</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Paypal Account Mode</label>
-                        <select name="paypal_account_mode" class="form-control select2">
-                            <option value="sandbox">Sandbox</option>
-                            <option value="live">Live</option>
-                        </select>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Account Mode</label>
+                            <select name="paypal_account_mode" class="form-control select2">
+                                <option @selected($paymentSetting->paypal_account_mode[0]['value'] === 'sandbox') value="sandbox">Sandbox</option>
+                                <option @selected($paymentSetting->paypal_account_mode[0]['value'] === 'live') value="live">Live</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Paypal Country Name</label>
-                        <select name="paypal_country" class="form-control select2">
-                            <option selected disabled value="">Choose</option>
-                            @foreach (config('country_list') as $key => $value)
-                                <option value="{{ $key }}">{{ $value }}</option>
-                            @endforeach
-                        </select>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Country Name</label>
+                            <select name="paypal_country" class="form-control select2">
+                                <option selected disabled value="">Choose</option>
+                                @foreach (config('country_list') as $key => $value)
+                                    <option @selected($paymentSetting->paypal_country[0]['value'] === $key) value="{{ $key }}">{{ $value }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Paypal Account Currency</label>
-                        <select name="paypal_account_currency" class="form-control select2">
-                            <option value="" disabled>Choose</option>
-                            @foreach (config('currencys.currency_list') as $currency )
-                            <option @selected(config('settings.site_default_currency') === $currency)  value="{{ $currency }}">{{ $currency }}</option>
-                            @endforeach
-                        </select>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Account Currency</label>
+                            <select name="paypal_account_currency" class="form-control select2">
+                                <option value="" disabled>Choose</option>
+                                @foreach (config('currencys.currency_list') as $currency)
+                                    <option @selected($paymentSetting->paypal_account_currency[0]['value'] === $currency) value="{{ $currency }}">{{ $currency }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Currency Rate Per ( {{ config('settings.site_default_currency') }} )</label>
-                        <input type="text" name="paypal_currency_rate" class="form-control"  value="" />
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Currency Rate Per ( {{ config('settings.site_default_currency') }} )</label>
+                            <input type="text" name="paypal_currency_rate" class="form-control"
+                                value="{{ $paymentSetting->paypal_currency_rate[0]['value'] }}" />
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Paypal Client Id</label>
-                        <input type="text" name="paypal_api_key" class="form-control"  value="" />
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Client Id</label>
+                            <input type="text" name="paypal_api_key" class="form-control"
+                                value="{{ $paymentSetting->paypal_api_key[0]['value'] }}" />
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Paypal Secret Key</label>
-                        <input type="text" name="paypal_secret_key" class="form-control"  value="" />
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Secret Key</label>
+                            <input type="text" name="paypal_secret_key" class="form-control"
+                                value="{{ $paymentSetting->paypal_secret_key[0]['value'] }}" />
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-12">
-                <div class="form-group">
-                    <label>Paypal Logo</label>
-                    <div id="image-preview" class="image-preview">
-                        <label for="image-upload" id="image-label">Choose File</label>
-                        <input type="file" name="paypal_image" id="image-upload" />
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Paypal Logo</label>
+                            <div id="image-preview" class="image-preview">
+                                <label for="image-upload" id="image-label">Choose File</label>
+                                <input type="file" name="paypal_image" id="image-upload" />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
-            </div>
-            <button type="submit" class="btn btn-primary">Save</button>
-        </form>
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
     </div>
 </div>
-</div>
+@section('js')
+    <script>
+        $(document).ready(function() {
+            var imagePath = '{{ asset($paymentSetting->paypal_image[0]["value"]) }}';
+
+            $('.image-preview').css({
+                'background-image': "url('" + imagePath + "')",
+                'background-size': 'cover',
+                'background-position': 'center center'
+            });
+
+        });
+    </script>
+@endsection

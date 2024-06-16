246ba9bf91b2c26b8e46cede256139e58e035b85
Mahmoud HabaZza <habazzaofficial@gmail.com>
2024-06-16 14:33:11 +0300

    236 - 8PayPal Integration Working with PayPal Integration Part 2

--- app/Repositories/EndUser/PaymentRepository.php
+++ app/Repositories/EndUser/PaymentRepository.php
@@ -36,13 +36,45 @@ class PaymentRepository implements PaymentRepositoryInterface {
 
         if($orderService->createOrder()){
             // redirect user to the selected payment gateway
-            return response(['redirect_url'=> route('paypal.payment')]);
+
+            switch ($request->payment_gateway) {
+                case 'paypal':
+                    return response(['redirect_url'=> route('paypal.payment')]);
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
+
+    public function setPaypalConfig(){
+        $config = [
+            'mode'    => config('gatewaySettings.paypal_account_mode'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+            'sandbox' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_key'),
+                'app_id'            => 'APP-80W284485P519543T',
+            ],
+            'live' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_key'),
+                'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
+            ],
+
+            'payment_action' => 'Sale', // Can only be 'Sale', 'Authorization' or 'Order'
+            'currency'       => config('gatewaySettings.paypal_account_currency'),
+            'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
+            'locale'         => 'en_US', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+            'validate_ssl'   => true, // Validate SSL when creating api client.
+        ];
+
+        return $config;
+    }
+
     public function payWithPaypal()
     {
-        
+
     }
     public function paypalSuccess()
     {
